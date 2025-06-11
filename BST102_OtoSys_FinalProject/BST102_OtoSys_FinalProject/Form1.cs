5b2547f1461863d7018c29594c01de593a82240f
emre keskin <vemrekes@hotmail.com>
2025-06-11 19:10:29 +0300

    Form 1 %70 tamamlandı.

--- BST102_OtoSys_FinalProject/BST102_OtoSys_FinalProject/Form1.cs
+++ BST102_OtoSys_FinalProject/BST102_OtoSys_FinalProject/Form1.cs
@@ -6,5 +6,25 @@ namespace BST102_OtoSys_FinalProject
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtpGirisTarihi_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtModel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
