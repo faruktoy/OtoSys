98764cca8a8f11938c2bfec3d721fa0df753a794
Faruk TOY <faruktoy231@hotmail.com>
2025-06-13 02:13:03 +0300

    Add.Login

---
+++ BST102_OtoSys_FinalProject/BST102_OtoSys_FinalProject/LoginForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BST102_OtoSys_FinalProject
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
